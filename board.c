4440c2f76c3604f239f72707b5dae561cd9deef5
Esha <dalale@mcmaster.ca>
2024-11-16 18:41:48 -0500

    Update board.c

--- board.c
+++ board.c
@@ -6,45 +6,41 @@ int surroundingMines(int rows, int columns, char minefield[rows][columns], int i
 	
 	int surrounding_mines = 0; 
 
-	for (int i = 0; i < rows; i++) { 
-		for (int j =0; j < columns; j++) { 
-			
-			if (minefield[i][j] != 'X') {
+	if (minefield[i][j] != 'X') {
 				
-				if (minefield[i+1][j] == 'X') { 
-					surrounding_mines++; 
-				}	
+		if (minefield[i+1][j] == 'X') { 
+			surrounding_mines++; 
+		}	
 
-				if (minefield[i-1][j] == 'X') { 
-					surrounding_mines++;
-				}
-	
-				if (minefield[i][j+1] == 'X') {
-                			surrounding_mines++;
-            			}
-
-				if (minefield[i][j-1] == 'X') {
-                			surrounding_mines++;
-				}
-
-				if (minefield[i-1][j+1] == 'X') { 
-                			surrounding_mines++;
-            			}
-			
-				if (minefield[i+1][j+1] == 'X') {
-                			surrounding_mines++;
-            			}
-
-				if (minefield[i-1][j-1] == 'X') {
-					surrounding_mines++;
-            			}
-
-				if (minefield[i+1][j-1] == 'X') {
-                			surrounding_mines++;
-            			}
-			}	
+		if (minefield[i-1][j] == 'X') { 
+			surrounding_mines++;
 		}
-}
+	
+		if (minefield[i][j+1] == 'X') {
+                	surrounding_mines++;
+            	}
+
+		if (minefield[i][j-1] == 'X') {
+                	surrounding_mines++;
+		}
+
+		if (minefield[i-1][j+1] == 'X') { 
+                	surrounding_mines++;
+            	}
+			
+		if (minefield[i+1][j+1] == 'X') {
+                	surrounding_mines++;
+            	}
+
+		if (minefield[i-1][j-1] == 'X') {
+			surrounding_mines++;
+            	}
+
+		if (minefield[i+1][j-1] == 'X') {
+                	surrounding_mines++;
+            	}
+	}	
+
 	return surrounding_mines; 
 }
 
@@ -71,18 +67,22 @@ void boardLayout(int rows, int columns, int mines) {
 	}
 
 	for (int i = 0; i < rows; i++) { // printing for dev purposes - need to print this when user selects a square 
-    	for (int j = 0; j < columns; j++) {
-    		printf("%c ", minefield[i][j]);
+    		for (int j = 0; j < columns; j++) {
+    			printf("%c ", minefield[i][j]);
+		}
+     	printf("\n");
+    }
+
+	for (int i = 0; i < rows; i++) { 
+		for (int j = 0; j < columns; j++) { 
 			if (minefield[i][j] != 'X') { 
 				int touching_mines = surroundingMines(rows, columns, minefield, i, j); 
 				printf("%d", touching_mines);
     			}
+		}
 	}
-     	printf("\n");
-    }
 
 }
-
 
 int main() { 
 
