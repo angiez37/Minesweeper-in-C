7d6520c4fce4375a24a6e303c441457578f7626d
Esha <dalale@mcmaster.ca>
2024-11-16 18:18:39 -0500

    Update board.c

--- board.c
+++ board.c
@@ -2,42 +2,8 @@
 #include <stdlib.h>
 #include <time.h> 
 
-
-void boardLayout(int rows, int columns, int mines) {
+int surroundingMines(int rows, int columns, char minefield[rows][columns], int i, int j) { // to-do - need to fix out-of-bounds errors (indexing)
 	
-	char minefield[rows][columns]; 
-	
-	int mines_placed = 0; 
-
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < columns; j++) {
-            minefield[i][j] = '.';
-        }
-    }
-
-	while (mines_placed < mines) { 
-		int i = rand() % rows;
-		int j = rand() % columns;
-		
-		if (minefield[i][j] != 'X') { 
-			minefield[i][j] = 'X'; 					
-			mines_placed++; 
-		}
-	}
-
-	for (int i = 0; i < rows; i++) {
-    	for (int j = 0; j < columns; j++) {
-    		printf("%c ", minefield[i][j]);
-			if (minefield[i][j] != 'X') { 
-				int touching_mines = surroundingMines(rows, columns, minefield, i, j); 
-    	}}
-     	printf("\n");
-    }
-
-}
-
-int surroundingMines(int rows, int columns, char minefield[rows][columns], int i, int j) { 
-
 	int surrounding_mines = 0; 
 
 	for (int i = 0; i < rows; i++) { 
@@ -54,37 +20,73 @@ int surroundingMines(int rows, int columns, char minefield[rows][columns], int i
 				}
 	
 				if (minefield[i][j+1] == 'X') {
-                	surrounding_mines++;
-            	}
+                			surrounding_mines++;
+            			}
 
 				if (minefield[i][j-1] == 'X') {
-                	surrounding_mines++;
+                			surrounding_mines++;
 				}
 
 				if (minefield[i-1][j+1] == 'X') { 
-                	surrounding_mines++;
-            	}
+                			surrounding_mines++;
+            			}
 			
 				if (minefield[i+1][j+1] == 'X') {
-                	surrounding_mines++;
-            	}
+                			surrounding_mines++;
+            			}
 
 				if (minefield[i-1][j-1] == 'X') {
 					surrounding_mines++;
-            	}
+            			}
 
 				if (minefield[i+1][j-1] == 'X') {
-                	surrounding_mines++;
-            	}
+                			surrounding_mines++;
+            			}
 			}	
 		}
 }
 	return surrounding_mines; 
 }
 
+void boardLayout(int rows, int columns, int mines) {
+	
+	char minefield[rows][columns]; 
+	
+	int mines_placed = 0; 
+
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < columns; j++) {
+	            minefield[i][j] = '.';
+	        }
+	    }
+
+	while (mines_placed < mines) { 
+		int i = rand() % rows;
+		int j = rand() % columns;
+		
+		if (minefield[i][j] != 'X') { 
+			minefield[i][j] = 'X'; 					
+			mines_placed++; 
+		}
+	}
+
+	for (int i = 0; i < rows; i++) { // printing for dev purposes - need to print this when user selects a square 
+    	for (int j = 0; j < columns; j++) {
+    		printf("%c ", minefield[i][j]);
+			if (minefield[i][j] != 'X') { 
+				int touching_mines = surroundingMines(rows, columns, minefield, i, j); 
+				print("%d", touching_mines);
+    			}
+	}
+     	printf("\n");
+    }
+
+}
+
+
 int main() { 
 
-    srand(time(NULL));
+	srand(time(NULL));
 
 	boardLayout(5, 7, 3);
 }
