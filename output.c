dcdc2163365865d141801d65e138948f20c6e433
Irxn21 <shahii8@mcmaster.ca>
2024-12-01 19:45:55 -0500

    updated defeat message

--- output.c
+++ output.c
@@ -7,6 +7,7 @@
 
 int endMenu(int state) {
     int choice;
+    int correct_input = 0;
     if (state == 1) {
         // Printing victory message (ignore format, when printed it works as expected)
         printf("   __        __    __    __ __ __    __ __ __     __ __ __     __ __    __    __\n");
@@ -22,21 +23,41 @@ int endMenu(int state) {
         printf("The mines have been defused, and peace has returned to the galaxy!\n");
     }
     else {
+        printf("   __ __ __     __ __ __    __ __ __    __ __ __         ___     __ __ __            \n");
+        printf("  |   __   \\   |   __ __|  |   __ __|  |   __ __|      / __ \\   |__    __|    \n");
+        printf("  |  |  \\   |  |  |__ __   |  |__ __   |  |__ __      / /__\\ \\     |  |       \n");
+        printf("  |  |   |  |  |   __ __|  |   __ __|  |   __ __|    /  ___   \\    |  |         \n");
+        printf("  |  |__/   |  |  |__ __   |  |        |  |__ __    /  /   \\   \\   |  |          \n");
+        printf("  |__ __ __/   |__ __ __|  |__|        |__ __ __|  /__/     \\ __\\  |__|            \n");
+
+        sleep(2);
+        printf("\n");
         printf("You have failed me for the last time, Commander.\n");
         printf("The mines have detonated, and the galaxy is in ruins.\n");
     }
+
     sleep(2);
     printf("\n");
     printf("1. Play Again\n");
     printf("2. Exit\n");
     printf("\n");
-    printf("Enter your choice:");
-    scanf("%d\n", &choice);
+    while (correct_input == 0) {
+        printf("Enter your choice:");
+        scanf("%d", &choice);
+        printf("\n");
+        if (choice == 1 || choice == 2) {
+            correct_input = 1;
+        }
+        else {
+            printf("Not in range! Pick either option 1 or 2.\n");
+        }
+    }
+    
     return choice;
 }
 
 
-void outputMinefield(char **minefield, int rows, int columns) { //char**
+void outputMinefield(char **minefield, int rows, int columns) { 
 
     printf("Location of Mines\n");
 
@@ -65,7 +86,7 @@ void outputMinefield(char **minefield, int rows, int columns) { //char**
 }
 
 // Testing purposes, remove before submitting
-int main() {
+/*int main() {
     // Sample minefield for testing
     int rows = 3, columns = 4;
     
@@ -79,7 +100,8 @@ int main() {
     // Call the function to print the minefield
     outputMinefield(minefield, rows, columns);
 
-    int test = endMenu(1);
+    int tc1 = endMenu(1);
+    printf("%d\n", tc1);
 
     return 0;
-}
+}*/
