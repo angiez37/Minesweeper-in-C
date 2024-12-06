ec6251de12bcf31615eb15ad18528eea48961b68
Esha <dalale@mcmaster.ca>
2024-12-05 19:11:50 -0500

    board test files

---
+++ board_tests/board_test.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "minesweeper.h"
+
+int main() {
+    srand(time(NULL));
+
+    int rows, columns, mines;
+
+    if (scanf("%d %d %d", &rows, &columns, &mines) != 3) {
+        fprintf(stderr, "Error: Invalid input format.\n");
+        return 1;
+    }
+
+    char** minefield = generateMinefield(rows, columns, mines);
+    if (!minefield) {
+        fprintf(stderr, "Error: Failed to generate minefield.\n");
+        return 1;
+    }
+
+    char** numberMap = generateNumberMap(rows, columns, minefield);
+    if (!numberMap) {
+        fprintf(stderr, "Error: Failed to generate number map.\n");
+        for (int i = 0; i < rows; i++) free(minefield[i]);
+        free(minefield);
+        return 1;
+   } 
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            printf("%c ", numberMap[i][j]);
+        }
+        printf("\n");
+    }
+
+   
+    for (int i = 0; i < rows; i++) {
+        free(minefield[i]);
+        free(numberMap[i]);
+    }
+    free(minefield);
+    free(numberMap);
+
+    return 0;
+}
+
